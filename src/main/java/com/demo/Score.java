722476bec7f92eecb16b2b2bd00b28e4d3cad688
shreyas1213 <shreyassshahapur007@gmail.com>
2022-12-11 19:30:44 +0000

    Score update

--- src/main/java/com/demo/Score.java
+++ src/main/java/com/demo/Score.java
@@ -1,17 +1,36 @@
 package com.demo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Score {
-    private static int Score;
+    private static int score;
+    private LocalDateTime timeStamp;
 
     public static void increaseScoreBy(int n) {
-        Score = Score + n;
+        score = score + n;
     }
 
     public static int getScore() {
-        return Score;
+        return score;
+    }
+
+
+    public String getFormatTimeStamp(){
+        return timeStamp.format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm"));
+    }
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public static Score createNew(int score){
+        Score newScore = new Score();
+        newScore.score = score;
+        newScore.timeStamp = LocalDateTime.now();
+        return newScore;
     }
 
     public static void setScore(int score) {
-        Score = score;
+        score = score;
     }
 }
