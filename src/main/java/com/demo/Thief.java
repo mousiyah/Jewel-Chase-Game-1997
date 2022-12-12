0a5b2e013686602ddbe3f0f8fc4d8442ecf80ffb
5fidel <119233141+5fidel@users.noreply.github.com>
2022-12-12 00:55:22 +0000

    javadoc

--- src/main/java/com/demo/Thief.java
+++ src/main/java/com/demo/Thief.java
@@ -1,9 +1,22 @@
 package com.demo;
 
+/**
+* 
+* @author 
+* @author Fidel Little 2123494
+* @author 
+* @version 2.0
+*/
+
 public abstract class Thief extends MovingEntity {
 
     protected Thief() {}
 
+    /**
+    * If there is an item on the tile it will collect it
+    * If there is a bomb on the tile it will activate it
+    * @param tile
+    */
     @Override
     protected void collectItem(Tile tile) {
         if (tile.getItem() != null) {
