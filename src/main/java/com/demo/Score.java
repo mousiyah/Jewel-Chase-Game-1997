f35b528a0f3c95245b339b42173c523c11b72f5b
5fidel <119233141+5fidel@users.noreply.github.com>
2022-12-12 00:57:00 +0000

    javadoc

--- src/main/java/com/demo/Score.java
+++ src/main/java/com/demo/Score.java
@@ -1,16 +1,35 @@
 package com.demo;
 
+/**
+* 
+* @author
+* @author
+* @version 2.0
+*/
+
+
 public class Score {
     private static int Score;
 
+    /**
+    *
+    * 
+    * @param score_increase
+    */
     public static void increaseScoreBy(int n) {
         Score = Score + n;
     }
 
+    /**
+    * @return score
+    */
     public static int getScore() {
         return Score;
     }
 
+    /**
+    * @param score
+    */
     public static void setScore(int score) {
         Score = score;
     }
