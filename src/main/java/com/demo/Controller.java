4d5ccbe353a8ba534e2e1685986baf123e20dae5
banksy2000 <118543807+banksy2000@users.noreply.github.com>
2022-12-12 00:30:16 +0000

    Added Javadoc
    Ready to add user name, student number, version number and method documentation.

--- src/main/java/com/demo/Controller.java
+++ src/main/java/com/demo/Controller.java
@@ -1,213 +1,70 @@
 package com.demo;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.animation.Timeline;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+/** Entity.java
+ * 
+ * @author user
+ * @version 2.0
+ */
+public abstract class Entity {
+    
+	private int x, y;
+    private static Tile[][] tiles;
+    protected ImageView imgView;
+    protected Image img;
+    private SnapshotParameters params;
+    private Timeline timeLine;
 
-public class Controller {
-
-    @FXML
-    Text score;
-
-    @FXML
-    Text levelScore;
-
-    @FXML
-    protected void changeScore() {
-        score.setText(Integer.toString(Score.getScore()));
+    protected Entity() {
+    
     }
 
-    @FXML
-    ProgressBar progressBar;
-    protected void changeProgressBar(int timeLeft, int timeLimit) {
-        float progress = (float) timeLeft / (float) timeLimit;
-        progressBar.setProgress(progress);
-        progressBar.setAccessibleText(timeLeft + " seconds");
+    protected void setPositions(int x, int y) {
+        setX(x);
+        setY(y);
     }
 
-    @FXML
-    protected void updateScore() {
-        levelScore.setText(Integer.toString(Score.getScore()));
-        Main.currentProfile.setScore(Main.currentProfile.getScore()+Score.getScore());
-        Profile.updateProfilesData();
+    public static Tile[][] getTiles() {
+        return tiles;
     }
 
-    @FXML
-    Text levelNumber;
-
-    @FXML
-    public void setLevelNumber(int n) {
-        levelNumber.setText(Integer.toString(n));
+    public static void setTiles(Tile[][] tiles) {
+        Entity.tiles = tiles;
     }
 
-    @FXML
-    public void replay(ActionEvent event) throws IOException {
-        Main.level.stopGame();
-        Main.openLevel(Main.currentLevel);
-        FileIO.deleteLevelState(Main.currentProfile.getName(),
-                Main.currentLevel);
+    public Timeline getTimeLine() {
+        return timeLine;
     }
 
-    @FXML
-    public void nextLevel(ActionEvent event) throws IOException {
-        if (Main.currentLevel <= Main.LEVELS) {
-            Main.currentLevel++;
-            Main.currentProfile.setMaxLevelUnlocked(Main.currentLevel);
-            Profile.updateProfilesData();
-            Main.openLevel(Main.currentLevel);
-        }
+    public void setTimeLine(Timeline timeLine) {
+        this.timeLine = timeLine;
     }
 
-    @FXML
-    public void goHome(ActionEvent event) throws IOException {
-        if (Main.level != null) {
-            Main.level.stopGame();
-        }
-        Main.stage.setScene(Main.createScene(new FXMLLoader(Main.fxmlHome)));
+    public int getX() {
+        return x;
     }
 
-    @FXML
-    public void goLevels(ActionEvent event) throws IOException {
-        Main.stage.setScene(Main.createScene(new FXMLLoader(Main.fxmlLevels)));
-        Main.controller.setLevelBtns();
-        Main.currentProfile.setLevelBtns(Main.controller);
+    public void setX(int x) {
+        this.x = x;
     }
 
-
-    @FXML
-    Button resumeGameBtn;
-
-    @FXML
-    Button pauseGameBtn;
-
-    @FXML
-    Button saveGameStateBtn;
-
-
-    @FXML
-    public void resumeGame(ActionEvent event) {
-        Main.level.resumeGame();
-        pauseGameBtn.setDisable(false);
-        resumeGameBtn.setDisable(true);
-        saveGameStateBtn.setDisable(true);
+    public int getY() {
+        return y;
     }
 
-    @FXML
-    public void pauseGame(ActionEvent event) {
-        Main.level.pauseGame();
-        pauseGameBtn.setDisable(true);
-        resumeGameBtn.setDisable(false);
-        saveGameStateBtn.setDisable(false);
+    public void setY(int y) {
+        this.y = y;
     }
 
-    @FXML
-    public void saveGameState(ActionEvent event) throws IOException {
-        Main.saveGameState();
-        goHome(event);
+    public Image getImg() {
+        return img;
     }
 
-    @FXML
-    public void newUserBtn(ActionEvent event) throws IOException {
-        Main.stage.setScene(Main.createScene(new FXMLLoader(Main.fxmlUserReg)));
+    public void setImg(Image img) {
+        this.img = img;
     }
-
-    @FXML
-    public void scoreBoardBtn(ActionEvent event) {
-    }
-
-    @FXML
-    ListView usersList;
-
-    @FXML
-    public void usersBtn(ActionEvent event) throws IOException {
-        Main.stage.setScene(Main.createScene(new FXMLLoader(Main.fxmlUsers)));
-        Main.setProfilesList(usersList);
-    }
-
-    @FXML
-    public void openLevelsScene() throws IOException {
-        if (usersList.getSelectionModel().getSelectedItem() != null) {
-            Main.openProfileLevels((String) usersList.getSelectionModel().getSelectedItem());
-        }
-    }
-
-    @FXML
-    public void deleteUser() throws IOException {
-        if (usersList.getSelectionModel().getSelectedItem() != null) {
-            Main.deleteProfile((String) usersList.getSelectionModel().getSelectedItem());
-            FileIO.deleteAllLevelStates((String) usersList.getSelectionModel().getSelectedItem());
-            Main.stage.setScene(Main.createScene(new FXMLLoader(Main.fxmlUsers)));
-            Main.setProfilesList(usersList);
-        }
-    }
-
-    @FXML
-    TextField newUserName;
-
-    @FXML
-    public void createNewUser(ActionEvent event) throws IOException {
-        if (!newUserName.getText().isEmpty()) {
-            Main.createNewProfile(newUserName.getText());
-        }
-
-    }
-
-    @FXML
-    Button levelbtn1;
-    @FXML
-    Button levelbtn2;
-    @FXML
-    Button levelbtn3;
-    @FXML
-    Button levelbtn4;
-    @FXML
-    Button levelbtn5;
-    @FXML
-    Button levelbtn6;
-
-    public ArrayList<Button> levelBtns;
-
-    public void setLevelBtns() {
-        levelBtns = new ArrayList<>();
-        levelBtns.add(levelbtn1);
-        levelBtns.add(levelbtn2);
-        levelBtns.add(levelbtn3);
-        levelBtns.add(levelbtn4);
-        levelBtns.add(levelbtn5);
-        levelBtns.add(levelbtn6);
-    }
-
-    public ArrayList<Button> getLevelBtns() {
-        return levelBtns;
-    }
-
-
-    @FXML
-    public void openLevel(ActionEvent event) throws IOException {
-        Button levelBtn = (Button) event.getTarget();
-        int levelNum = Integer.parseInt(levelBtn.getId().
-                replaceAll("levelbtn", ""));
-
-        File file = null;
-        if (Main.currentProfile != null) {
-            file = FileIO.levelStateExists(Main.currentProfile.getName(),
-                    String.valueOf(levelNum));
-        }
-
-        if (file == null) {
-            Main.openLevel(levelNum);
-        } else {
-            Main.playGameFromSavedState(file, levelNum);
-        }
-    }
-
 }
