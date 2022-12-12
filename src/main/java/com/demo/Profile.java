25296661e2a35a06d9306ccb982b774c4a44c60b
5fidel <119233141+5fidel@users.noreply.github.com>
2022-12-12 00:58:52 +0000

    javadoc

--- src/main/java/com/demo/Profile.java
+++ src/main/java/com/demo/Profile.java
@@ -1,11 +1,26 @@
 package com.demo;
 import java.io.IOException;
 
+/**
+* 
+* @author
+* @author
+* @author 
+* @version 2.0
+*/
+
 public class Profile {
     private String name;
     private int maxLevelUnlocked;
     private int score;
 
+    /**
+    *
+    * 
+    * @param name
+    * @throws IOException
+    * @see IOException
+    */
     public Profile(String name) throws IOException {
         this.name = name;
         maxLevelUnlocked = 1;
@@ -13,21 +28,45 @@ public class Profile {
         saveProfileData();
     }
 
+    /**
+    *
+    * 
+    * @param name
+    * @param level_reached
+    * @param score
+    * @throws IOException
+    * @see IOException
+    */
     public Profile(String name, int maxLevelUnlocked, int score) throws IOException {
         this.name = name;
         this.maxLevelUnlocked = maxLevelUnlocked;
         this.score = score;
     }
 
+    /**
+    *
+    * 
+    */
     public static void updateProfilesData() {
         FileIO.writeProfiles();
     }
 
+    /**
+    *
+    * 
+    */
     public void saveProfileData() {
         FileIO.addToTheFile("profiles.txt",
                 "%user% " + name + " 1 0" + "\n");
     }
 
+    /**
+    *
+    * 
+    * @param controller
+    * @throws IOException
+    * @see IOException
+    */
     public void setLevelBtns(Controller controller) throws IOException {
         for (int i = 0; i < controller.getLevelBtns().size(); i++) {
             if (i+1 <= maxLevelUnlocked) {
@@ -39,26 +78,44 @@ public class Profile {
 
     }
 
+    /**
+    * @return name
+    */
     public String getName() {
         return name;
     }
 
+    /**
+    * @param name
+    */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+    * @return max level reached by player
+    */
     public int getMaxLevelUnlocked() {
         return maxLevelUnlocked;
     }
 
+    /**
+    * @param max level reached by player
+    */
     public void setMaxLevelUnlocked(int maxLevelUnlocked) {
         this.maxLevelUnlocked = maxLevelUnlocked;
     }
 
+    /**
+    * @return score
+    */
     public int getScore() {
         return score;
     }
 
+    /**
+    * @param score
+    */
     public void setScore(int score) {
         this.score = score;
     }
